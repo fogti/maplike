d6d020e3b2151823bf61d259e862d1bd8659556a
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-03-03 00:15:20 +0100

    Add trait and default method to clear any collection

--- src/lib.rs
+++ src/lib.rs
@@ -80,6 +80,22 @@ pub trait Pop: KeyedCollection {
     fn pop(&mut self) -> Option<Self::Value>;
 }
 
+/// Remove all elements from the collection.
+pub trait Clear<K>: Clone + IntoIter<K> + Remove<K>
+where
+    Self::IntoIter: DoubleEndedIterator,
+{
+    /// Remove all elements from the collection.
+    fn clear(&mut self) {
+        let keys: _alloc::vec::Vec<K> =
+            self.clone().into_iter().rev().map(|(key, _)| key).collect();
+
+        for key in keys {
+            self.remove(&key);
+        }
+    }
+}
+
 /// Returns the length of the collection.
 ///
 /// Should be only implemented for truly contiguous data structures, for which
