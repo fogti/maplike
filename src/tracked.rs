e3bf48f0c91464d62b6b5ce748ce8d6569a0f55c
Έλλεν Εμίλια Άννα Zscheile <fogti+devel@ytrizja.de>
2026-08-31 15:15:57 +0200

    feat(tracks): Make `Tracks` generic over sidetracks container

--- src/tracked.rs
+++ src/tracked.rs
@@ -8,28 +8,30 @@ use crate::{
     containers::Container,
     ops::{Assign, Get, Len, Modify, Set, WithOne},
 };
-use alloc_::{boxed::Box, vec::Vec};
 use core::{
     iter::{Chain, Once},
     ops::Deref,
 };
 
-/// `Tracks`, a non-empty list.
+/// `Tracks`, a primary/main track with an associated container of other tracks
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct Tracks<S> {
+pub struct Tracks<S, C> {
     /// The main element
     pub primary: S,
 
     /// The other elements
-    pub parallels: Box<[S]>,
+    pub sidetracks: C,
 }
 
-impl<S> Tracks<S> {
-    /// Creates a new non-empty list
+impl<S, C> Tracks<S, C> {
+    /// Creates a new `Tracks` collection
     #[inline(always)]
-    pub const fn new(primary: S, parallels: Box<[S]>) -> Self {
-        Self { primary, parallels }
+    pub const fn new(primary: S, sidetracks: C) -> Self {
+        Self {
+            primary,
+            sidetracks,
+        }
     }
 
     /// Returns a reference to the main element
@@ -47,38 +49,51 @@ impl<S> Tracks<S> {
 
     /// Returns a reference to the tail
     #[inline(always)]
-    pub const fn parallels(&self) -> &[S] {
-        &self.parallels
+    pub const fn sidetracks(&self) -> &C {
+        &self.sidetracks
     }
+}
 
+impl<S, C> Tracks<S, C> {
     /// Creates a borrowed version of this non-empty list
     #[inline]
-    pub fn as_ref(&self) -> Tracks<&S> {
+    pub fn as_ref<'s, C2>(&'s self) -> Tracks<&'s S, C2>
+    where
+        &'s C: IntoIterator<Item = &'s S>,
+        C2: FromIterator<&'s S>,
+    {
         Tracks {
             primary: &self.primary,
-            parallels: self.parallels.iter().collect(),
+            sidetracks: (&self.sidetracks).into_iter().collect(),
         }
     }
 
     /// Creates an unborrowed version of this non-empty list
     #[inline(always)]
-    pub fn as_deref(&self) -> Tracks<&<S as Deref>::Target>
+    pub fn as_deref<'s, C2>(&'s self) -> Tracks<&'s <S as Deref>::Target, C2>
     where
         S: Deref,
+        &'s C: IntoIterator<Item = &'s S>,
+        C2: FromIterator<&'s <S as Deref>::Target>,
     {
         self.map_ref(Deref::deref)
     }
 
     /// Maps `Tracks<S>` to `Tracks<T>` by applying a function to the contained values.
     #[inline]
-    pub fn map<T, F>(self, mut f: F) -> Tracks<T>
+    pub fn map<T, C2, F>(self, mut f: F) -> Tracks<T, C2>
     where
         F: FnMut(S) -> T,
+        C: IntoIterator<Item = S>,
+        C2: FromIterator<T>,
     {
-        let Tracks { primary, parallels } = self;
+        let Tracks {
+            primary,
+            sidetracks,
+        } = self;
         Tracks {
             primary: f(primary),
-            parallels: Vec::from(parallels).into_iter().map(f).collect(),
+            sidetracks: sidetracks.into_iter().map(f).collect(),
         }
     }
 
@@ -86,131 +101,167 @@ impl<S> Tracks<S> {
     ///
     /// Equivalent to `self.as_ref().map(f)`, but potentially faster.
     #[inline]
-    pub fn map_ref<'s, T, F>(&'s self, mut f: F) -> Tracks<T>
+    pub fn map_ref<'s, T, C2, F>(&'s self, mut f: F) -> Tracks<T, C2>
     where
         F: FnMut(&'s S) -> T,
+        &'s C: IntoIterator<Item = &'s S>,
+        C2: FromIterator<T>,
     {
-        let Tracks { primary, parallels } = self;
+        let Tracks {
+            primary,
+            sidetracks,
+        } = self;
         Tracks {
             primary: f(primary),
-            parallels: parallels.iter().map(f).collect(),
+            sidetracks: sidetracks.into_iter().map(f).collect(),
         }
     }
 
     /// Applies a modification in-place, and returns `Tracks<T>` of the return values.
     #[inline]
-    pub fn modify_inplace<'s, T, F>(&'s mut self, mut f: F) -> Tracks<T>
+    pub fn modify_inplace<'s, T, C2, F>(&'s mut self, mut f: F) -> Tracks<T, C2>
     where
         F: FnMut(&'s mut S) -> T,
+        &'s mut C: IntoIterator<Item = &'s mut S>,
+        C2: FromIterator<T>,
     {
-        let Tracks { primary, parallels } = self;
+        let Tracks {
+            primary,
+            sidetracks,
+        } = self;
         Tracks {
             primary: f(primary),
-            parallels: parallels.iter_mut().map(f).collect(),
+            sidetracks: sidetracks.into_iter().map(f).collect(),
         }
     }
 }
 
-impl<S> Container for Tracks<S> {
-    type Key = usize;
-    type Value = S;
+impl<C: Container> Container for Tracks<C::Value, C> {
+    type Key = Option<C::Key>;
+    type Value = C::Value;
 }
 
-impl<S> WithOne<S> for Tracks<S> {
+impl<C> WithOne<C::Value> for Tracks<C::Value, C>
+where
+    C: Container + Default,
+{
     #[inline(always)]
-    fn with_one(primary: S) -> Self {
+    fn with_one(primary: C::Value) -> Self {
         Self {
             primary,
-            parallels: Vec::new().into_boxed_slice(),
+            sidetracks: Default::default(),
         }
     }
 }
 
-impl<S> Assign for Tracks<S> {
+impl<C: Container> Assign for Tracks<C::Value, C> {
     #[inline(always)]
     fn assign(&mut self, value: Self) {
         *self = value;
     }
 }
 
-impl<S> Get<usize> for Tracks<S> {
+impl<C, K> Get<Option<K>> for Tracks<C::Value, C>
+where
+    C: Container + Get<K>,
+{
     #[inline]
-    fn get(&self, index: &usize) -> Option<&S> {
-        if let Some(index_prev) = (*index).checked_sub(1) {
-            self.parallels.get(index_prev)
+    fn get(&self, index: &Option<K>) -> Option<&C::Value> {
+        if let Some(index_prev) = index {
+            self.sidetracks.get(index_prev)
         } else {
             Some(&self.primary)
         }
     }
 }
 
-impl<S> Tracks<S> {
-    /// Returns a mutable reference to the value corresponding to the key.
+impl<C, K> Set<Option<K>> for Tracks<<C as Container>::Value, C>
+where
+    C: Container + Set<K>,
+    <C as Set<K>>::Output: From<C::Value>,
+{
+    type Output = <C as Set<K>>::Output;
+
     #[inline]
-    pub fn get_mut(&mut self, index: usize) -> Option<&mut S> {
-        if let Some(index_prev) = index.checked_sub(1) {
-            self.parallels.get_mut(index_prev)
-        } else {
-            Some(&mut self.primary)
+    fn set(&mut self, index: Option<K>, value: C::Value) -> <C as Set<K>>::Output {
+        use core::mem::replace;
+        match index {
+            None => replace(&mut self.primary, value).into(),
+            Some(index_prev) => self.sidetracks.set(index_prev, value),
         }
     }
 }
 
-impl<S> Set<usize> for Tracks<S> {
-    type Output = Option<S>;
-
+impl<C, K> Modify<Option<K>> for Tracks<C::Value, C>
+where
+    C: Container + Modify<K>,
+{
     #[inline]
-    fn set(&mut self, index: usize, value: S) -> Option<S> {
-        Some(core::mem::replace(self.get_mut(index)?, value))
-    }
-}
-
-impl<S> Modify<usize> for Tracks<S> {
-    #[inline]
-    fn modify<F>(&mut self, index: &usize, f: F)
+    fn modify<F>(&mut self, index: &Option<K>, mut f: F)
     where
-        F: FnOnce(&mut S),
+        F: for<'a> FnMut(&'a mut C::Value),
     {
-        f(self.get_mut(*index).unwrap())
+        match index {
+            None => f(&mut self.primary),
+            Some(index_prev) => self.sidetracks.modify(index_prev, f),
+        }
     }
 }
 
-impl<S> Len for Tracks<S> {
+impl<C: Len> Len for Tracks<C::Value, C> {
     #[inline(always)]
     fn len(&self) -> usize {
-        1 + self.parallels.len()
+        1 + self.sidetracks.len()
     }
 }
 
-impl<S> IntoIterator for Tracks<S> {
-    type Item = S;
-    type IntoIter = Chain<Once<S>, alloc_::vec::IntoIter<S>>;
+impl<C> IntoIterator for Tracks<C::Item, C>
+where
+    C: IntoIterator,
+{
+    type Item = C::Item;
+    type IntoIter = Chain<Once<C::Item>, C::IntoIter>;
 
     #[inline]
     fn into_iter(self) -> Self::IntoIter {
-        let Tracks { primary, parallels } = self;
-        core::iter::once(primary).chain(Vec::from(parallels))
+        let Tracks {
+            primary,
+            sidetracks,
+        } = self;
+        core::iter::once(primary).chain(sidetracks)
     }
 }
 
-impl<'a, S> IntoIterator for &'a Tracks<S> {
+impl<'a, S, C> IntoIterator for &'a Tracks<S, C>
+where
+    &'a C: IntoIterator<Item = &'a S>,
+{
     type Item = &'a S;
-    type IntoIter = Chain<Once<&'a S>, core::slice::Iter<'a, S>>;
+    type IntoIter = Chain<Once<&'a S>, <&'a C as IntoIterator>::IntoIter>;
 
     #[inline]
     fn into_iter(self) -> Self::IntoIter {
-        let Tracks { primary, parallels } = self;
-        core::iter::once(primary).chain(&parallels[..])
+        let Tracks {
+            primary,
+            sidetracks,
+        } = self;
+        core::iter::once(primary).chain(sidetracks)
     }
 }
 
-impl<'a, S> IntoIterator for &'a mut Tracks<S> {
+impl<'a, S, C> IntoIterator for &'a mut Tracks<S, C>
+where
+    &'a mut C: IntoIterator<Item = &'a mut S>,
+{
     type Item = &'a mut S;
-    type IntoIter = Chain<Once<&'a mut S>, core::slice::IterMut<'a, S>>;
+    type IntoIter = Chain<Once<&'a mut S>, <&'a mut C as IntoIterator>::IntoIter>;
 
     #[inline]
     fn into_iter(self) -> Self::IntoIter {
-        let Tracks { primary, parallels } = self;
-        core::iter::once(primary).chain(&mut parallels[..])
+        let Tracks {
+            primary,
+            sidetracks,
+        } = self;
+        core::iter::once(primary).chain(sidetracks)
     }
 }
